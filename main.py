85e218931f50e01bbc8dbbe979b972e54bc14b12
di-was <diwasdahal109@gmail.com>
2022-06-24 18:35:14 +0545

    Project Layout Completed

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+#IMAGE_EXTENSIONS = ['.jpg', '.png', '.webp', '.gif']
+#VIDEO_EXTENSIONS = ['.mp4']
+# Previous Variables initialized in case the project steers toward file catorization. 
+
+def main():
+    # check integrity
+    # filter out the uneccessary files
+    # iterate over the folder which contains images and rename them
+    pass
+
